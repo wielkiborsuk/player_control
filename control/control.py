8e08a305006aa81c61a86af5488a9602c5070747
wielki.borsuk <wielki.borsuk@gmail.com>
2020-02-10 15:45:45 +0100

    add chromium play/pause support with process identification

--- control/control.py
+++ control/control.py
@@ -1,5 +1,6 @@
 from subprocess import check_output
 import dbus
+import re
 
 
 class Controller(object):
@@ -73,28 +74,14 @@ class CmusController(Controller):
         return output
 
 
-class SpotifyController(Controller):
-    name = 'spotify'
-    __dbus_name = 'org.mpris.MediaPlayer2.spotify'
+class MprisController(Controller):
     __dbus_player = 'org.mpris.MediaPlayer2.Player'
     __dbus_path = '/org/mpris/MediaPlayer2'
     __dbus_properties = 'org.freedesktop.DBus.Properties'
     __bus = dbus.SessionBus()
-    __player_object = __bus.get_object(__dbus_name, __dbus_path)
-    __player_properties = dbus.Interface(__player_object,
-                                         dbus_interface=__dbus_properties)
-    __player_interface = dbus.Interface(__player_object,
-                                        dbus_interface=__dbus_player)
 
-    def status(self):
-        status = self.__spotify_metadata()
-        title = status['xesam:title']
-        duration = int(status['mpris:length'])
-        status_message = '{} {}/{}'.format(
-            title,
-            self.format_time(self.__spotify_position()),
-            self.format_time(duration//1000000))
-        return status_message
+    def __init__(self, dbus_name):
+        self.__dbus_name = dbus_name
 
     def toggle(self):
         return self.__player_interface.PlayPause()
@@ -103,24 +90,73 @@ class SpotifyController(Controller):
         return self.__player_interface.Next()
 
     def is_active(self):
-        status = self.__spotify_playback_status()
+        status = self.__mpris_playback_status()
         return status == 'Playing'
 
-    def __spotify_metadata(self):
+    def status(self):
+        status = self.mpris_metadata()
+        title = status['xesam:title']
+        duration = int(status.get('mpris:length', '0'))
+        status_message = '{} {}/{}'.format(
+            title,
+            self.format_time(self.mpris_position()),
+            self.format_time(duration//1000000))
+        return status_message
+
+    def mpris_metadata(self):
         return self.__player_properties.Get(self.__dbus_player,
                                             'Metadata')
 
-    def __spotify_playback_status(self):
-        return self.__player_properties.Get(self.__dbus_player,
-                                            'PlaybackStatus')
-
-    def __spotify_position(self):
+    def mpris_position(self):
         return self.__player_properties.Get(self.__dbus_player,
                                             'Position')
 
+    def __mpris_playback_status(self):
+        return self.__player_properties.Get(self.__dbus_player,
+                                            'PlaybackStatus')
 
-class BrowserController(Controller):
-    name = 'browser'
+    def __get_player_object(self):
+        return self.__bus.get_object(self.__dbus_name, self.__dbus_path)
 
-    def status(self):
-        return 'browser'
+    @property
+    def __player_properties(self):
+        return dbus.Interface(
+            self.__get_player_object(),
+            dbus_interface=self.__dbus_properties)
+
+    @property
+    def __player_interface(self):
+        return dbus.Interface(
+            self.__get_player_object(),
+            dbus_interface=self.__dbus_player)
+
+    def find_instance(self, query):
+        obj = self.__bus.get_object("org.freedesktop.DBus",
+                                    "/org/freedesktop/DBus")
+        result = [str(name) for name in obj.ListNames() if query in name]
+        return result
+
+
+class SpotifyController(MprisController):
+    name = 'spotify'
+    __dbus_name = 'org.mpris.MediaPlayer2.spotify'
+
+    def __init__(self):
+        super().__init__(self.__dbus_name)
+
+
+class BrowserController(MprisController):
+    name = 'chromium'
+
+    def __init__(self):
+        instances = self.find_instance(self.name)
+        dbus_name = self.__select_instance_by_cmd(instances, 'chromium')
+        super().__init__(dbus_name)
+
+    def __select_instance_by_cmd(self, instances, cmd_part):
+        for instance in instances:
+            pid = re.match('.+instance(\\d+)', instance).group(1)
+            cmd = check_output(['ps', '-o', 'cmd=', pid]).decode('utf-8')
+            if cmd_part in cmd:
+                return instance
+        return None
