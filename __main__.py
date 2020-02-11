af9afb39bc0880169014785e31ebca0ded75c827
wielki.borsuk <wielki.borsuk@gmail.com>
2020-02-11 13:17:07 +0100

    add last player cache, scan for active, handle commandline

--- __main__.py
+++ __main__.py
@@ -1,12 +1,34 @@
+import argparse
 from control.control import DelegatingController
 
 
+def configure_argparser():
+    parser = argparse.ArgumentParser(description='Player cli controller')
+    parser.add_argument('command', type=str,
+                        help='command for the player to execute')
+    parser.add_argument('--target', type=str, dest='target',
+                        help='target player name to override current')
+
+    return parser
+
+
+def execute(controller, command):
+    if hasattr(controller, command):
+        result = getattr(controller, command)()
+        if result:
+            print(result)
+    else:
+        print('unknown command')
+
+
 def main():
+    parser = configure_argparser()
+    args = parser.parse_args()
     controller = DelegatingController()
-    controller.focus('cmus')
-    # print(controller.status())
-    # controller.next()
-    controller.toggle()
+
+    if args.target:
+        controller.focus(args.target)
+    execute(controller, args.command)
 
 
 if __name__ == "__main__":
--- control/control.py
+++ control/control.py
@@ -9,6 +9,8 @@ class Controller(object):
 
 
 class DelegatingController(Controller):
+    tmp_file = '/tmp/player_control_current'
+
     def __init__(self):
         self.current = None
         self.controllers = [
@@ -16,11 +18,22 @@ class DelegatingController(Controller):
             SpotifyController(),
             BrowserController()
         ]
+        self.focus(self.__load_focus())
+        self.scan()
 
     def focus(self, name):
         for controller in self.controllers:
             if controller.name == name:
                 self.current = controller
+                self.__save_focus(name)
+
+    def scan(self):
+        for controller in self.controllers:
+            try:
+                if controller.is_active():
+                    self.focus(controller.name)
+            except Exception:
+                print('controller {} not responding'.format(controller.name))
 
     def get_controler(self):
         return self.current or self.controllers[0]
@@ -33,6 +46,17 @@ class DelegatingController(Controller):
 
     def next(self):
         return self.get_controler().next()
+
+    def __save_focus(self, name):
+        with open(self.tmp_file, 'w') as state:
+            state.write(name)
+
+    def __load_focus(self):
+        try:
+            with open(self.tmp_file, 'r') as state:
+                return state.read()
+        except Exception:
+            return None
 
 
 class CmusController(Controller):
