f2f2291faa7d5f74affefd602e800ffd776b91fc
wielki.borsuk <wielki.borsuk@gmail.com>
2024-01-12 23:44:34 +0100

    avoid matching firefox/chrome instance pid if there's only one

--- control/control.py
+++ control/control.py
@@ -206,6 +206,8 @@ class BrowserController(MprisController):
         super().__init__(dbus_name)
 
     def __select_instance_by_cmd(self, instances, cmd_part):
+        if len(instances) == 1:
+            return instances[0]
         for instance in instances:
             pid = re.match('.+instance(\\d+)', instance).group(1)
             cmd = check_output(['ps', '-o', 'cmd=', pid]).decode('utf-8')
@@ -223,8 +225,10 @@ class FirefoxController(MprisController):
         super().__init__(dbus_name)
 
     def __select_instance_by_cmd(self, instances, cmd_part):
+        if len(instances) == 1:
+            return instances[0]
         for instance in instances:
-            pid = re.match('.+instance(\\d+)', instance).group(1)
+            pid = re.match('.+instance([_\\d]+)', instance).group(1)
             cmd = check_output(['ps', '-o', 'cmd=', pid]).decode('utf-8')
             if cmd_part in cmd:
                 return instance
